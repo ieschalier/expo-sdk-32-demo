6ae3e27d23d347a48ae904815c7d607ec0715991
Igor ESCHALIER <igor.eschalier@gmail.com>
2019-01-10 17:21:57 +0100

    try catch api requests

--- screens/Location/index.js
+++ screens/Location/index.js
@@ -19,16 +19,22 @@ import { MonoText } from '../../components/StyledText'
 import { DENIED, GRANTED, GRANTED_IN_USE, WAIT, BACKGROUND_UPDATE, BACKGROUND_GEOFENCE } from '../Constants'
 
 TaskManager.defineTask(BACKGROUND_UPDATE, async ({ data: { locations }, error }) => {
-  await Promise.all(locations.map(async ({ coords }) => {
-    const result = await fetch(`http://192.168.1.14:3000/location/${coords.longitude}/${coords.latitude}`)
-  }))
+  try {
+    await Promise.all(locations.map(async ({ coords }) => {
+      const result = await fetch(`http://192.168.1.14:3000/location/${coords.longitude}/${coords.latitude}`)
+    }))
+  } catch (error) {
+  }
 })
 
 TaskManager.defineTask(BACKGROUND_GEOFENCE, ({ data: { eventType, region }, error }) => {
   const inMoscow = region.identifier === 'moscow' && eventType === Location.GeofencingEventType.Enter
 
   store.set('inMoscow', inMoscow)
-  if (inMoscow) fetch(`http://192.168.1.14:3000/location/moscow`)
+  try {
+    if (inMoscow) fetch(`http://192.168.1.14:3000/location/moscow`)
+  } catch (error) {
+  }
 })
 
 class Loc extends React.Component {
